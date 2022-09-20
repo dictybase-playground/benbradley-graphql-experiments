67092bd008df86f34fc4aae87f23dba53d7393ce
bbradley <36743188+benbradley747@users.noreply.github.com>
2022-09-20 14:41:40 -0500

    Fetch data from spacex graphql server
     - Successfully fetched data from graphql server
     - Populated a table with fetched data

---
+++ src/components/rocket-datagrid.tsx
@@ -0,0 +1,42 @@
+import { Maybe, Rocket, RocketsResult } from "../generated/graphql";
+import Table from "@mui/material/Table";
+import TableBody from "@mui/material/TableBody";
+import TableCell from "@mui/material/TableCell";
+import TableContainer from "@mui/material/TableContainer";
+import TableHead from "@mui/material/TableHead";
+import TableRow from "@mui/material/TableRow";
+import Paper from "@mui/material/Paper";
+
+function RocketDataGrid({ data }: RocketsResult) {
+  return (
+    <TableContainer component={Paper}>
+      <Table sx={{ minWidth: 650 }} aria-label="simple table">
+        <TableHead>
+          <TableRow>
+            <TableCell align="left">Name</TableCell>
+            <TableCell align="left">Country</TableCell>
+            <TableCell align="left">Description</TableCell>
+            <TableCell align="left">Wikipedia</TableCell>
+          </TableRow>
+        </TableHead>
+        <TableBody>
+          {data?.map((row: Maybe<Rocket>) => (
+            <TableRow
+              key={row?.id}
+              sx={{ "&:last-child td, &:last-child th": { border: 0 } }}
+            >
+              <TableCell component="th" scope="row">
+                {row?.name}
+              </TableCell>
+              <TableCell align="left">{row?.country}</TableCell>
+              <TableCell align="left">{row?.description}</TableCell>
+              <TableCell align="left">{row?.wikipedia}</TableCell>
+            </TableRow>
+          ))}
+        </TableBody>
+      </Table>
+    </TableContainer>
+  );
+}
+
+export default RocketDataGrid;
