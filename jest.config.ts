1a9c26dcb3156754a328d3c77c2808a4c66fba5f
bbradley <36743188+benbradley747@users.noreply.github.com>
2022-09-23 12:45:24 -0500

    @swc/jest config issue

---
+++ jest.config.ts
@@ -0,0 +1,11 @@
+/*
+ * For a detailed explanation regarding each configuration property and type check, visit:
+ * https://jestjs.io/docs/configuration
+ */
+
+export default {
+  testEnvironment: "jsdom",
+  transform: {
+    '^.+\\.(t|j)sx?$': "@swc/jest",
+  },
+};
