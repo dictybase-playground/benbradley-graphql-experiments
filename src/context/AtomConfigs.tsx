c9afdec938cfa2b1412f0b3a1a36499ccdb7629a
Ben Bradley <ben.bradley747@gmail.com>
2022-10-10 14:01:28 -0500

    feat: launch atom config

--- src/context/AtomConfigs.tsx
+++ src/context/AtomConfigs.tsx
@@ -1,5 +1,6 @@
 import { atom } from 'jotai'
 
+// Rocket Query atom configs
 export const limitOptions = ['1', '2', '3', '4']
 
 export interface RocketQueryState {
@@ -20,4 +21,24 @@ export const rocketLimitIntAtom = atom((get) =>
 
 export const rocketFilterAtom = atom<RocketQueryState>({
   filter: '',
+})
+
+// Launch Query atom configs
+export const launchLimitOptions = ['10', '20', '30', '40']
+
+export const launchLimitAtom = atom(
+  launchLimitOptions[launchLimitOptions.length - 1],
+  (get, set, update) => {
+    set(launchLimitAtom, update)
+    get(launchLimitAtom)
+  },
+)
+
+export const launchLimitIntAtom = atom((get) =>
+  Number.parseInt(get(launchLimitAtom), 10),
+)
+
+export const launchFilterAtom = atom('', (get, set, update) => {
+  set(launchFilterAtom, update)
+  get(launchFilterAtom)
 })
