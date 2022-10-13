f6f8f580152eca30b5f834fb108e846b2a6b9fd4
Ben Bradley <ben.bradley747@gmail.com>
2022-10-13 18:56:35 -0500

    test: type and size selectors

--- src/tests/QueryTypeSelector.test.tsx
+++ src/tests/QueryTypeSelector.test.tsx
@@ -1,33 +1,38 @@
 import { render, screen } from '@testing-library/react'
 import userEvent from '@testing-library/user-event'
-import QuerySizeSelector from '../components/QuerySizeSelector'
 import '@testing-library/jest-dom'
+import QueryTypeSelector from '../components/QueryTypeSelector'
 
-describe('components/QuerySizeSelector', () => {
-  const renderComponent = () => render(<QuerySizeSelector />)
+describe('components/QueryTypeSelector', () => {
+  const renderComponent = () => render(<QueryTypeSelector />)
 
-  it('should render a size dropdown selector', async () => {
+  it('should render a type dropdown selector', () => {
     renderComponent()
 
     expect(
       screen.getByRole('button', {
-        name: /size/i,
+        name: /query/i,
       }),
     ).toBeInTheDocument()
   })
 
-  it('should render size option when clicked', async () => {
+  it('should render type option when clicked', async () => {
     renderComponent()
 
     const sizeDropdown = screen.getByRole('button', {
-      name: /size/i,
+      name: /query/i,
     })
 
     await userEvent.click(sizeDropdown)
 
     expect(
       screen.getByRole('option', {
-        name: /5/i,
+        name: /launch/i,
+      }),
+    ).toBeInTheDocument()
+    expect(
+      screen.getByRole('option', {
+        name: /rocket/i,
       }),
     ).toBeInTheDocument()
   })
