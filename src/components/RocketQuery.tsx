662b9a30192e688585e48877ea0b8739556a1d0b
Ben Bradley <ben.bradley747@gmail.com>
2022-10-13 23:49:53 -0500

    chore: swapped rocket header for generic header
    - updated test code

--- src/components/RocketQuery.tsx
+++ src/components/RocketQuery.tsx
@@ -1,12 +1,13 @@
 import { CircularProgress } from '@mui/material'
-import { useAtom } from 'jotai'
+import { useAtom, useAtomValue } from 'jotai'
 import { Rocket, useGetRocketsQuery } from '../generated/graphql'
 import RocketDataGrid from './RocketDataGrid'
 import RocketQueryError from './QueryError'
-import { queryLimitIntAtom } from '../context/AtomConfigs'
+import { queryLimitIntAtom, queryTypeAtom } from '../context/AtomConfigs'
 
 const RocketQuery = () => {
   const [limit] = useAtom(queryLimitIntAtom)
+  const { headers } = useAtomValue(queryTypeAtom)
   const { data, loading, error } = useGetRocketsQuery({
     variables: { limit },
   })
@@ -16,7 +17,10 @@ const RocketQuery = () => {
       {loading ? <CircularProgress /> : undefined}
       {error ? <RocketQueryError error={error} /> : undefined}
       {data ? (
-        <RocketDataGrid rocketData={data.rockets as Rocket[]} />
+        <RocketDataGrid
+          rocketHeaders={headers}
+          rocketData={data.rockets as Rocket[]}
+        />
       ) : undefined}
     </>
   )
