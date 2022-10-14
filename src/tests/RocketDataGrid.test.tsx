5ed9927b8ff53996b358e5f96217985d2c7f62e9
Ben Bradley <ben.bradley747@gmail.com>
2022-10-13 20:02:32 -0500

    test: launch/rocket datagrid

--- src/tests/RocketDataGrid.test.tsx
+++ src/tests/RocketDataGrid.test.tsx
@@ -2,15 +2,45 @@ import React from 'react'
 import '@testing-library/jest-dom'
 import { render, screen } from '@testing-library/react'
 import RocketDataGrid from '../components/RocketDataGrid'
+import { Rocket } from '../generated/graphql'
+
+const testRockets: Rocket[] = [
+  {
+    id: '1',
+    name: 'RU Rocket',
+    wikipedia: 'wikilink',
+    description: 'Description',
+    country: 'Russia',
+    active: true,
+  },
+  {
+    id: '2',
+    name: 'US Rocket',
+    wikipedia: 'wikilink',
+    description: 'Description',
+    country: 'United States',
+    active: true,
+  },
+  {
+    id: '3',
+    name: 'EU Rocket',
+    wikipedia: 'wikilink',
+    description: 'Description',
+    country: 'Europe',
+    active: true,
+  },
+]
 
 describe('components/RocketDataGrid', () => {
-  it('should render a table component', async () => {
-    render(<RocketDataGrid rocketData={[]} />)
+  it('should render a table component', () => {
+    render(<RocketDataGrid rocketData={testRockets} />)
 
-    const header = screen.getByRole('columnheader', {
-      name: /name/i,
-    })
+    expect(screen.getByRole('table')).toBeInTheDocument()
+  })
 
-    expect(header).toBeInTheDocument()
+  it('should render correct amount of rows', () => {
+    render(<RocketDataGrid rocketData={testRockets} />)
+
+    expect(screen.getAllByRole('row')).toHaveLength(testRockets.length + 1)
   })
 })
