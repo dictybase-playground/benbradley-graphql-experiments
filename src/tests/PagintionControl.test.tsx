ebc3c6ce07a6acf25f763b81bdc5347ec48bff22
Ben Bradley <ben.bradley747@gmail.com>
2022-10-17 12:28:24 -0500

    test: pagination control next button increments count

--- src/tests/PagintionControl.test.tsx
+++ src/tests/PagintionControl.test.tsx
@@ -1,7 +1,18 @@
+/* eslint-disable @typescript-eslint/no-extra-semi */
 import { render, screen } from '@testing-library/react'
-// import userEvent from '@testing-library/user-event'
+import userEvent from '@testing-library/user-event'
 import PaginationControl from '../components/PaginationControl'
+import { useGetLaunchesQuery } from '../generated/graphql'
 import '@testing-library/jest-dom'
+import mockLaunches from '../mocks/mockLaunches'
+
+jest.mock('../generated/graphql', () => {
+  // eslint-disable-next-line no-shadow
+  const useGetLaunchesQuery = jest.fn()
+  return { useGetLaunchesQuery }
+})
+
+const testPageSize = 10
 
 describe('components/QueryFilter', () => {
   it('should render pagination control buttons', () => {
@@ -14,7 +25,37 @@ describe('components/QueryFilter', () => {
       name: /next/i,
     })
 
+    // check page number
+    expect(screen.getByText(/1/)).toBeInTheDocument()
     expect(previousButton).toBeInTheDocument()
+    expect(previousButton).toBeDisabled()
     expect(nextButton).toBeInTheDocument()
+  })
+
+  it('increments page number when clicking next button', async () => {
+    ;(useGetLaunchesQuery as jest.Mock).mockReturnValue({
+      loading: false,
+      error: false,
+      data: mockLaunches,
+    })
+
+    render(
+      <PaginationControl
+        pageItemLimit={testPageSize}
+        totalItems={mockLaunches.launches?.length as number}
+      />,
+    )
+
+    const previousButton = screen.getByRole('button', {
+      name: /previous/i,
+    })
+    const nextButton = screen.getByRole('button', {
+      name: /next/i,
+    })
+
+    await userEvent.click(nextButton)
+
+    expect(previousButton).toBeEnabled()
+    expect(screen.getByText(/2/)).toBeInTheDocument()
   })
 })
